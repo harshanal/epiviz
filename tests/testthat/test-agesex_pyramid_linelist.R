fd696f911cabe9af3a6d198d3b8ce6bbead45783
Harshana Liyanage <Harshana.Liyanage@ukhsa.gov.uk>
2024-03-24 10:18:29 +0000

    add new tests for age sex pyramid

---
+++ tests/testthat/test-agesex_pyramid_linelist.R
@@ -0,0 +1,12 @@
+# test to check if the function returns a ggplot object
+
+test_that("agesex_pyramid_linelist works", {
+
+  library(epiviz)
+
+  # check that the output is a ggplot object
+  expect_true(inherits(agesex_pyramid_linelist(lab_data), "ggplot"))
+  expect_true(inherits(agesex_pyramid_linelist(lab_data, x_breaks=10), "ggplot"))
+  expect_true(inherits(agesex_pyramid_linelist(lab_data, x_breaks=10, conf_limits = TRUE), "ggplot"))
+
+})
