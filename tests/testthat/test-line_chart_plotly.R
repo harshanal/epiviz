aca3e029d7781ca79e436a3637bb0dc5d698b234
Harshana Liyanage <Harshana.Liyanage@ukhsa.gov.uk>
2024-03-24 01:10:40 +0000

    update test file with changed column names

--- tests/testthat/test-line_chart_plotly.R
+++ tests/testthat/test-line_chart_plotly.R
@@ -10,21 +10,21 @@ test_that("line_chart_plotly works", {
   test_df <- epiviz::lab_data
 
   #manipulating date within df
-  test_df$Specimen_Date <- as.Date(test_df$Specimen_Date)
+  test_df$specimen_date <- as.Date(test_df$specimen_date)
 
   ## setting start date and end date for aggregation
   start_date <- as.Date("2023-01-01")
   end_date <- as.Date("2023-12-31")
 
   #summarisation
-  summarised_df <- test_df  %>% group_by(Organism_Species_Name, Specimen_Date) %>%
+  summarised_df <- test_df  %>% group_by(organism_species_name, specimen_date) %>%
     summarize(count = n(), .groups = 'drop') %>% ungroup()%>%
-    filter(Specimen_Date >= start_date & Specimen_Date<= end_date)
+    filter(specimen_date >= start_date & specimen_date<= end_date)
 
   #This matches with all the mandatory variable needed to produce a basic
   #line graph
-  result <- line_chart_plotly(df = summarised_df, x= "Specimen_Date", y = "count",
-                              grouping_col = "Organism_Species_name")
+  result <- line_chart_plotly(df = summarised_df, x= "specimen_date", y = "count",
+                              grouping_col = "organism_species_name")
 
   # check that the output is a plotly object
   expect_true(inherits(result, "plotly"))
