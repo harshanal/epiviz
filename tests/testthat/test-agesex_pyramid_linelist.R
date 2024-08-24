e072471090617482453a15db0c967f9df334dae2
Harshana Liyanage <Harshana.Liyanage@ukhsa.gov.uk>
2024-08-24 18:55:08 +0100

    replaced magrittr pipe with native pipe

--- tests/testthat/test-agesex_pyramid_linelist.R
+++ tests/testthat/test-agesex_pyramid_linelist.R
@@ -5,8 +5,8 @@ test_that("agesex_pyramid_linelist works", {
   library(epiviz)
 
   # check that the output is a ggplot object
-  expect_true(inherits(agesex_pyramid_linelist(lab_data), "ggplot"))
-  expect_true(inherits(agesex_pyramid_linelist(lab_data, x_breaks=10), "ggplot"))
-  expect_true(inherits(agesex_pyramid_linelist(lab_data, x_breaks=10, conf_limits = TRUE), "ggplot"))
+  expect_true(inherits(agesex_pyramid_linelist(epiviz::lab_data), "ggplot"))
+  expect_true(inherits(agesex_pyramid_linelist(epiviz::lab_data, x_breaks=10), "ggplot"))
+  expect_true(inherits(agesex_pyramid_linelist(epiviz::lab_data, x_breaks=10, conf_limits = TRUE), "ggplot"))
 
 })
--- tests/testthat/test-point_chart.R
+++ tests/testthat/test-point_chart.R
@@ -1,11 +1,11 @@
 
 
 test_that("point_chart produces static chart", {
-
+  library(epiviz)
   # Create test dataframe from epiviz::lab_data
-  data <- lab_data %>%
-    group_by(specimen_month = lubridate::floor_date(specimen_date, 'month')) %>%
-    summarise(detections = n()) %>%
+  data <- epiviz::lab_data |>
+    group_by(specimen_month = lubridate::floor_date(specimen_date, 'month')) |>
+    summarise(detections = n()) |>
     ungroup()
 
   # Create params list
@@ -36,11 +36,11 @@ test_that("point_chart produces static chart", {
 
 
 test_that("point_chart produces dynamic chart", {
-
+  library(epiviz)
   # Create test dataframe from epiviz::lab_data
-  data <- lab_data %>%
-    group_by(specimen_month = lubridate::floor_date(specimen_date, 'month')) %>%
-    summarise(detections = n()) %>%
+  data <- epiviz::lab_data |>
+    group_by(specimen_month = lubridate::floor_date(specimen_date, 'month')) |>
+    summarise(detections = n()) |>
     ungroup()
 
   # Create params list
@@ -71,9 +71,9 @@ test_that("point_chart produces dynamic chart", {
 
 
 test_that("point_chart produces static chart with grouped data", {
-
+  library(epiviz)
   # Create test dataframe from epiviz::lab_data
-  data <- lab_data |>
+  data <- epiviz::lab_data |>
     group_by(specimen_month = lubridate::floor_date(specimen_date, 'month'),
              organism_species_name) |>
     summarise(detections = n()) |>
@@ -122,9 +122,9 @@ test_that("point_chart produces static chart with grouped data", {
 
 
 test_that("point_chart produces dynamic chart with grouped data", {
-
+  library(epiviz)
   # Create test dataframe from epiviz::lab_data
-  data <- lab_data |>
+  data <- epiviz::lab_data |>
     group_by(specimen_month = lubridate::floor_date(specimen_date, 'month'),
              organism_species_name) |>
     summarise(detections = n()) |>
